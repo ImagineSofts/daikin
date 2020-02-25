20387155af398b706a8f05506556fe84b4634552
shanewas ahmed nabil <shanewasahmed@gmail.com>
2020-02-25 18:21:07 +0600

    initial commit

---
+++ src/views/LandingPage/Sections/ProductSection.js
@@ -0,0 +1,67 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+import Chat from "@material-ui/icons/Chat";
+import VerifiedUser from "@material-ui/icons/VerifiedUser";
+import Search from "@material-ui/icons/Search";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import InfoArea from "components/InfoArea/InfoArea.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function ProductSection() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={8}>
+          <h2 className={classes.title}>
+            Enjoy your perfect climate with Daikin
+          </h2>
+          <h5 className={classes.description}>
+            The way we heat and cool our homes is changing. Discover how to
+            create your perfect climate at home, with energy efficient solutions
+            that minimise your environmental impact.
+          </h5>
+        </GridItem>
+      </GridContainer>
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Get in Touch"
+              description="Chatting system for users"
+              icon={Chat}
+              iconColor="info"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Login to My DAIKIN"
+              description="Login portal"
+              icon={VerifiedUser}
+              iconColor="success"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Find your Product"
+              description="Find your product"
+              icon={Search}
+              iconColor="danger"
+              vertical
+            />
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
+}
---
+++ src/views/LandingPage/Sections/WorkSection.js
@@ -0,0 +1,73 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPageSections/workStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function WorkSection() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer justify="center">
+        <GridItem cs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Home Owner Support</h2>
+          <h4 className={classes.description}>
+            We want you to be able to depend on Daikin for the ultimate in
+            comfort, so we offer dedicated support lines and a range of
+            maintenance packages, together with a trusted network of skilled
+            service engineers to help you get the most from your Daikin system.
+          </h4>
+          <form>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <CustomInput
+                  labelText="Your Name"
+                  id="name"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={6}>
+                <CustomInput
+                  labelText="Your Email"
+                  id="email"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                />
+              </GridItem>
+              <CustomInput
+                labelText="Your Message"
+                id="message"
+                formControlProps={{
+                  fullWidth: true,
+                  className: classes.textArea
+                }}
+                inputProps={{
+                  multiline: true,
+                  rows: 5
+                }}
+              />
+              <GridContainer justify="center">
+                <GridItem xs={12} sm={12} md={4} className={classes.textCenter}>
+                  <Button color="primary">Send Message</Button>
+                </GridItem>
+              </GridContainer>
+            </GridContainer>
+          </form>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
